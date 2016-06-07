7e3202f326a6631bb8e91831aba2ae40f769a3c2
loek-tonnaer <loek.tonnaer@gmail.com>
2016-06-07 22:06:43 +0200

    Added vorticity confinement
    Seems to work well. Code may need some cleaning up.

--- Solver.h
+++ Solver.h
@@ -24,6 +24,7 @@ private:
 	void lin_solve(int N, int b, float * x, float * x0, float a, float c);
 	void project(int N, float * u, float * v, float * p, float * div);
 	void set_bnd(int N, int b, float * x);
+	void confine_vorticity(int N, float * u, float * v);
 };
 
 #endif /* SOLVER_H */
